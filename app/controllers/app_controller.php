760d8e362e2dac2b0de460cad242f1c944051dcc
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2014-07-03 11:42:25 +1000

    1

---
+++ app/controllers/app_controller.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MichaelZ
+ * Date: 2/07/14
+ * Time: 5:26 PM
+ */
+class AppController extends Controller{
+    var $components = array('DebugKit.Toolbar');
+}
